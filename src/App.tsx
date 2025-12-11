d18f10ace4c2e698652166265051eb2d07ecaf18
ArunveerSidhu <sidhu.arunveer@gmail.com>
2025-12-11 18:14:32 +0530

    chore: make folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Home } from './pages'
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <Home />
     </>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Post, Comments } from '../components'
+
+function Home() {
+  return (
+    <div>
+      <Post />
+      <Comments />
+    </div>
+  )
+}
+
+export default Home
